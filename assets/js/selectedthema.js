4dc539ccb5ac4ff53b7c263a65e5006ec30c7a8c
Muhammet Hamza İnce <hamzaince10@gmail.com>
2024-02-29 23:55:29 +0300

    thema edit js

--- assets/js/selectedthema.js
+++ assets/js/selectedthema.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var totalSlides = document.querySelectorAll('.swiper-slide').length;
+
     var mySwiper = new Swiper('.swiper-container', {
         direction: 'horizontal',
         autoplay: {
@@ -10,7 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        loop: false
+        loop: true,
+        slidesPerView: 3,
+        loopedSlides: totalSlides,
     });
 });
 
@@ -18,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var section = document.getElementById('section4');
     var button = document.getElementById('previewThema');
 
-    button.style.display = 'none'; // Butonu başlangıçta gizle
+    button.style.display = 'none';
 
     function toggleButton() {
         if (window.scrollY >= section.offsetTop) {
@@ -30,3 +34,42 @@ document.addEventListener("DOMContentLoaded", function() {
 
     window.addEventListener('scroll', toggleButton);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var swiperSlides = document.querySelectorAll('.swiper-slide');
+
+    swiperSlides.forEach(function (slide) {
+        var image = slide.querySelector('img');
+
+        var selectedDiv = slide.querySelector('.selected');
+        selectedDiv.style.display = 'none';
+
+        var selectedSlide = null;
+        var isSelected = false;
+
+        image.addEventListener('click', function () {
+            if (selectedSlide === slide) {
+                isSelected = !isSelected;
+
+                if (!isSelected) {
+                    selectedDiv.style.display = 'none';
+                    selectedSlide = null;
+                }
+            } else {
+                if (selectedSlide !== null) {
+                    selectedSlide.querySelector('.selected').style.display = 'none';
+                }
+
+                selectedSlide = slide;
+                isSelected = true;
+
+                selectedDiv.style.display = 'flex';
+            }
+        });
+    });
+});
+
+
+
+
